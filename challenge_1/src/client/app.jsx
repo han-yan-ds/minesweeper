166b912395e4c8671e88ef9e47eeda811f501910
han-yan-ds <hanprogramming@gmail.com>
2019-09-30 17:39:24 -0600

    Refactored to have paginate in separate component as App, stylable.  Now pages are horizontally listed

--- challenge_1/src/client/app.jsx
+++ challenge_1/src/client/app.jsx
@@ -1,44 +1,18 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import Search from './components/Search.jsx';
 import ResultsList from './components/ResultsList.jsx';
-import {setSearchResultAction} from './actions/actions';
-import {fetchResults} from './util';
-import ReactPaginate from 'react-paginate';
+import Paginate from './components/Paginate.jsx';
+import './styles/app.css';
 
-function mapStateToProps (state) {
-  const {searchQuery, numPaginatedPages} = state;
-  return {searchQuery, numPaginatedPages};
-}
-
-function mapDispatchToProps (dispatch) {
-  return {
-    changePage: async (data, searchQuery) => {
-      let page = data.selected+1;
-      let resjson = await fetchResults(searchQuery, page);
-      dispatch(setSearchResultAction(resjson));
-    },
-  }
-}
-
-function App ({searchQuery, numPaginatedPages, changePage}) {
+function App () {
   return (
     <div>
       <Search/>
       <ResultsList/>
-      <ReactPaginate
-        previousLabel={'prev'}
-        nextLabel={'next'}
-        breakLabel={'...'}
-        pageCount={numPaginatedPages}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={(data) => changePage(data, searchQuery)}
-        activeClassName={'active'}
-      />
+      <Paginate/>
     </div>
   );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
 
