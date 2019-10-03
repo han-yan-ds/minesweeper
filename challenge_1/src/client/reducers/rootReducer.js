643aaaae0e4345583f66961cc8adbaa87a590571
han-yan-ds <hanprogramming@gmail.com>
2019-10-03 17:57:50 -0600

    Reorganized folder structure for this redone repo

--- challenge_1/src/client/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-// import other reducers
-import {searchResult, searchQuery, numPaginatedPages} from './reducers';
-
-export default combineReducers({
-  searchResult,
-  searchQuery,
-  numPaginatedPages,
-});
