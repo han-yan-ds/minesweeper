bd6a3136b0048f61adf20c154a23b9ebe537c396
han-yan-ds <hanprogramming@gmail.com>
2019-10-04 15:30:43 -0600

    Added Difficulty Selection Box

--- components/NewGameButton.jsx
+++ components/NewGameButton.jsx
@@ -1,3 +1,4 @@
+import {Component} from 'react';
 import {connect} from 'react-redux';
 import boardInitializer from '../util/boardInitializer';
 import {newGameAction} from '../redux/actions/actions';
@@ -8,13 +9,41 @@ function mapDispatchToProps (dispatch) {
   }
 }
 
-function NewGameButton ({newGame}) {
-  let width = 10;
-  let height = 10;
-  let numMines = 10;
-  return (
-    <button onClick={() => newGame(width, height, numMines)}>NEW GAME</button>
-  );
+class NewGameButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      difficulty: 0,
+    }
+  }
+
+  render() {
+    let difficulties = [
+      {width: 10, height: 10, numMines: 10},
+      {width: 15, height: 15, numMines: 27},
+      {width: 20, height: 20, numMines: 55},
+      {width: 25, height: 25, numMines: 100},
+    ];
+    let context = this;
+    return (
+      <form>
+        <select onChange={(e) => this.setState({difficulty: e.target.value})}>
+          <option value={0}>Novice</option>
+          <option value={1}>Intermediate</option>
+          <option value={2}>Expert</option>
+          <option value={3}>Nightmare</option>
+        </select>
+        <br/><br/>
+        <button onClick={(e) => {
+          e.preventDefault();
+          context.props.newGame(
+            difficulties[context.state.difficulty].width,
+            difficulties[context.state.difficulty].height,
+            difficulties[context.state.difficulty].numMines,
+          );}}>NEW GAME</button>
+      </form>
+    );
+  }
 }
 
 export default connect(null, mapDispatchToProps)(NewGameButton);
