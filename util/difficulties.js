c59b357e12bea3243c45650424179dbafe4e9837
han-yan-ds <hanprogramming@gmail.com>
2019-10-12 20:17:55 -0600

    Fixed Responsiveness to work for screens of width 800

--- util/difficulties.js
+++ util/difficulties.js
@@ -2,5 +2,5 @@ export default [
   {width: 10, height: 10, numMines: 10},
   {width: 15, height: 15, numMines: 27},
   {width: 20, height: 20, numMines: 55},
-  {width: 25, height: 25, numMines: 100},
+  {width: 22, height: 30, numMines: 100},
 ];
